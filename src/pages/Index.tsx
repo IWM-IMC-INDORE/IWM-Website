a961cd5dbedf5a47166c7899f32371f780688762
DevKan:PC <kanhaagrawal272@gmail.com>
2025-12-25 00:51:56 +0530

    added carousel

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,11 +4,13 @@ import { AboutSection } from "@/components/home/AboutSection";
 import { DepartmentsSection } from "@/components/home/DepartmentsSection";
 import { TestimonialsSection } from "@/components/home/TestimonialsSection";
 import { CTASection } from "@/components/home/CTASection";
+import { HomeGallery } from "@/components/home/HomeGallery";
 
 const Index = () => {
   return (
     <PublicLayout>
       <HeroSection />
+      <HomeGallery />
       <AboutSection />
       <DepartmentsSection />
       <TestimonialsSection />
